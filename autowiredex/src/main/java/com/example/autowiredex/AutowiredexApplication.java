b920f2a32e458e79a5e7d9d9b6001697821ee0ee
g m mortoza ahmed <mortozafsti@gmail.com>
2019-02-05 13:06:52 +0600

    java Update

---
+++ autowiredex/src/main/java/com/example/autowiredex/AutowiredexApplication.java
@@ -0,0 +1,14 @@
+package com.example.autowiredex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutowiredexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutowiredexApplication.class, args);
+    }
+
+}
+
---
+++ autowiredex/src/main/java/com/example/autowiredex/Test/autowiredtest.java
@@ -0,0 +1,7 @@
+package com.example.autowiredex.Test;
+
+public class autowiredtest {
+    public static void main(String[] args) {
+
+    }
+}
