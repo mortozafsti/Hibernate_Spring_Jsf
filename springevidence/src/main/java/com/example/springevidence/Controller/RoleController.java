abf1f234415b0b161cbf5b60aabaaf9ef70429c3
g m mortoza ahmed <mortozafsti@gmail.com>
2019-02-11 14:38:08 +0600

    java Update

--- springevidence/src/main/java/com/example/springevidence/Controller/RoleController.java
+++ springevidence/src/main/java/com/example/springevidence/Controller/RoleController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
@@ -47,9 +48,12 @@ public class RoleController {
         }
         return "roleAdd";
     }
-//    @GetMapping(value = "/del/{id}")
-//    public String add(Role role){
-//        return "roleAdd";
-//    }
+    @GetMapping(value = "/delr/{id}")
+    public String deleterole(Model model, @PathVariable("id") Long id){
+        if (id != null){
+            this.rolerepo.deleteById(id);
+        }
+        return "redirect:/role";
+    }
 
 }
