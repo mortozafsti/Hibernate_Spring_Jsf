9fa8506afb6fc4b3792bcfa168485381d95ea5ff
G M Mortoza Ahmed <mortozafsti@gmail.com>
2019-02-15 02:52:52 +0600

    java update

---
+++ templating/src/test/java/com/custom/templating/TemplatingApplicationTests.java
@@ -0,0 +1,17 @@
+package com.custom.templating;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TemplatingApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
