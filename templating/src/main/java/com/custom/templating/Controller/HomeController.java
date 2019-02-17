3248aa71ef6d342112eee0312bbfafb0098b4dec
g m mortoza ahmed <mortozafsti@gmail.com>
2019-02-17 11:47:24 +0600

    java Update

--- templating/src/main/java/com/custom/templating/Controller/HomeController.java
+++ templating/src/main/java/com/custom/templating/Controller/HomeController.java
@@ -11,7 +11,7 @@ public class HomeController {
         return "index";
     }
 
-    @GetMapping(value = "/add-rolee")
+    @GetMapping(value = "/add-role")
     public String displayRole() {
         return "add-role";
     }
--- templating/src/main/java/com/custom/templating/Controller/RoleController.java
+++ templating/src/main/java/com/custom/templating/Controller/RoleController.java
@@ -5,19 +5,34 @@ import com.custom.templating.repo.RoleRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
+
 @Controller
-@RequestMapping(value = "/role/")
+@RequestMapping(value = "/user/")
 public class RoleController {
 
     @Autowired
     private RoleRepo roleRepo;
 
-    @GetMapping(value = "add-rolee")
+    @GetMapping(value = "/add-role")
     public String addRole(Model model){
         model.addAttribute("obj", new Role());
         return "add-role";
     }
+
+    @PostMapping(value = "/add-role")
+    public String roleSave(@Valid Role role, BindingResult result,Model model){
+
+        this.roleRepo.save(role);
+        model.addAttribute("role", new Role());
+
+        return "add-role";
+
+    }
+
 }
