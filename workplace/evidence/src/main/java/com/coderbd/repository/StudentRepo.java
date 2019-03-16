7f9e2d3b7bf1436fa435c5cf6b2695e174c952be
g m mortoza ahmed <mortozafsti@gmail.com>
2019-03-16 14:36:27 +0600

    java Update

---
+++ workplace/evidence/src/main/java/com/coderbd/repository/StudentRepo.java
@@ -0,0 +1,16 @@
+package com.coderbd.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.coderbd.entity.Student;
+
+@Repository
+public interface StudentRepo extends JpaRepository<Student, Long>{
+
+
+ Student findByEmail(String email);
+
+
+	
+}
