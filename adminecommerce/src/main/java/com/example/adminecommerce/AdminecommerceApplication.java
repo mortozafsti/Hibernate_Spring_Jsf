9a5f3645d12042fec4086c46031e483629f81d87
G M Mortoza Ahmed <mortozafsti@gmail.com>
2019-05-30 20:40:06 +0600

    java update

---
+++ adminecommerce/src/main/java/com/example/adminecommerce/AdminecommerceApplication.java
@@ -0,0 +1,13 @@
+package com.example.adminecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminecommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminecommerceApplication.class, args);
+    }
+
+}
---
+++ adminecommerce/src/test/java/com/example/adminecommerce/AdminecommerceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.adminecommerce;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AdminecommerceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
